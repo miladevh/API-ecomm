9e97dc5bc8d8d954d5eb00ad69f134e5254d9cf3
milad hatami <miladhatami.dev@gmail.com>
2024-05-20 17:32:46 +0330

    Changing the database to Postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecomm',
+        'USER': 'postgres',
+        'PASSWORD': 'Md0011',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
