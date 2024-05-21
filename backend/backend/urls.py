9ac925cc644a2d3463467b1cb4e013f2836c88aa
milad hatami <miladhatami.dev@gmail.com>
2024-05-21 16:35:03 +0330

    add home app to setting and base urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('', include('home.urls', namespace='home')),
 ]
